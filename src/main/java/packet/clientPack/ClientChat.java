051e08812464e8b5d5e710d09166ef885a0bc553
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-09-25 08:58:46 +0500

    test - connected;

---
+++ src/main/java/packet/clientPack/ClientChat.java
@@ -0,0 +1,52 @@
+package packet.clientPack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+// Клиент устанавливает соединение с  сервером по адресу и порту
+// Считываем сообщение с консоли и отправляем на сервер
+// Прослушиваем сообщения от сервера и отправляем в консоль
+// Клиент запускается в цикле до "exit"
+
+public class ClientChat {
+    private Socket socket = null; // для установки связи с сервером
+    private BufferedReader inputConsole = null; // выходной поток сообщений с записью в буфер
+    private PrintWriter out = null; // отправка сообщений
+    private BufferedReader in = null; // получение ответа от сервера
+
+    public ClientChat(String address,int port){
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected to the chat server");
+
+            inputConsole = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(socket.getOutputStream(),true);
+            in = new BufferedReader(new InputStreamReader((socket.getInputStream())));
+
+            String line = "";
+            while (line.equals("exit")){ // прослушивание ответов от сервера в цикле
+                line = inputConsole.readLine();
+                out.println(line);
+                System.out.println(in.readLine()); // и вывод
+            }
+            socket.close();
+            inputConsole.close();
+            out.close();
+
+
+        }catch (UnknownHostException u){
+            System.out.println("Host unknown " + u.getMessage());
+        }catch (IOException i){
+            System.out.println("Unexpected exception " + i.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        ClientChat clientChat = new ClientChat("127.0.0.1",50728);
+    }
+
+}
--- src/main/java/packet/clientPack/ClientHandler.java
+++ src/main/java/packet/clientPack/ClientHandler.java
@@ -7,16 +7,19 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.List;
 
-public class ClientHandler {
-    private Socket clientSocket;
-    private List<ClientHandler> clients;
-    private PrintWriter out;
+
+// отслеживание списка всех подключенных клиентов
+public class ClientHandler implements Runnable{
+    private Socket clientSocket; // сокет
+    private List<ClientHandler> clients; // список клиентов
+    private PrintWriter out; /// вывод
 
     /* PrintWriter - это подкласс. Writer, который используется для печати форматированных данных в OutputStream
     или другой. Writer, которым он управляет. */
 
-    private BufferedReader in;
+    private BufferedReader in; // считывание входящих клиентов
 
+    // конструктор передачи значений
     public ClientHandler(Socket socket,List<ClientHandler> clients) throws IOException {
         this.clientSocket = socket;
         this.clients = clients;
@@ -28,9 +31,9 @@ public class ClientHandler {
         try {
 
             String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                for (ClientHandler aClient : clients) {
-                    aClient.out.println(inputLine);
+            while ((inputLine = in.readLine()) != null) { // если во входящей линии есть клиенты
+                for (ClientHandler aClient : clients) { // перебираем их
+                    aClient.out.println(inputLine); // и выводим
                 }
             }
         }catch (IOException e){
